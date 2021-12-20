1b6d37f8acb9ab5b6a958a9741dbc96b6df0c5e9
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-20 20:37:41 +0600

    seed data added

--- medusa-config.js
+++ medusa-config.js
@@ -7,7 +7,7 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-    process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+    process.env.DATABASE_URL || "postgres://sayeed:1990@localhost/diesel_demo";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
@@ -37,8 +37,8 @@ module.exports = {
         // For more production-like environment install PostgresQL
         database_url: DATABASE_URL,
         database_type: "postgres",
-        //database_database: "./medusa-db.sql",
-        //database_type: "sqlite",
+        // database_database: "./medusa-db.sql",
+        // database_type: "sqlite",
         store_cors: STORE_CORS,
         admin_cors: ADMIN_CORS,
         database_extra:
